990dd68f5a35ff700254c88c1bb596a87987d3a6
revisionismer <cherry2137@naver.com>
2023-05-14 18:28:27 +0900

    21. 컨트롤러 레이어 본코드 - 책저장하기

--- src/main/java/com/library/web/dto/CMRespDto.java
+++ src/main/java/com/library/web/dto/CMRespDto.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 public class CMRespDto<T> {
 
-	private int code;  // 1(성공), -1(실패)
+	private Integer code;  // 1(성공), -1(실패)
 	private String message;
 	private T data;
 }
